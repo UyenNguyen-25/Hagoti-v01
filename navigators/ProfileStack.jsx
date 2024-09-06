220d8552de834fce20611e5855caf8e3061ef831
LuuAiGiao <luugiao.lm2003@gmail.com>
2024-09-06 22:21:43 +0700

    profile screen

---
+++ navigators/ProfileStack.jsx
@@ -0,0 +1,12 @@
+import { createStackNavigator } from '@react-navigation/stack';
+import Profile from '../app/(tabs)/profile';
+
+const Stack = createStackNavigator();
+
+export default function ProfileStack() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="profile" component={Profile} />
+    </Stack.Navigator>
+  );
+}
